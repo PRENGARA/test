231f46fe8c398800966ba2b3cd0801b6da268bac
PRENGARA <prengara@depaul.edu>
2025-05-16 14:07:26 -0500

    Add files via upload

--- BadCode.java
+++ BadCode.java
@@ -1,9 +1,9 @@
-// File: BadCode.java
 public class BadCode {
     public static void main(String[] args) {
-        int unused = 5; // Unused variable
+        int unused = 42; // Unused variable
         try {
-            int x = 1 / 0;
-        } catch (Exception e) {} // Empty catch block
+            int result = 10 / 0;
+        } catch (Exception e) {
+        } // Empty catch block
     }
 }
